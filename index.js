9e04fc6a9fec5882a29aa5adcf2f87fc3b06b373
Myles Starr <mylesstarr@gmail.com>
2021-03-24 18:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object.key = value
+  return object
+}
